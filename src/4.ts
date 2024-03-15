8428511b32e88c8a129548413f2c5a8fd2c17c09
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2024-03-15 11:01:10 +0200

    4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,63 @@
+class Key {
+  private signature: string;
+
+  constructor() {
+    this.signature = Math.random().toString();
+  }
+
+  public getSignature(): string {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person has come in.");
+    } else {
+      console.log("Door is closed. Cannot enter.");
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  public openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log("Door is opened.");
+    } else {
+      console.log("Wrong key. Door remains closed.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +66,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
